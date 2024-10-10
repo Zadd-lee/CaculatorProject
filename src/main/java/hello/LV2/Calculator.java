e867522416cbe2236a7445bb9ed827ede8681f63
이자드 <kimminkyeong0418@gmail.com>
2024-10-10 13:41:42 +0900

    refactor: LV2 변수명, 주석 설명 추가

--- src/main/java/hello/LV2/Calculator.java
+++ src/main/java/hello/LV2/Calculator.java
@@ -15,32 +15,38 @@ public class Calculator {
         this.resultHistory = resultHistory;
     }
 
-    public Integer Calculator(int a, int b, char operater) {
+    public Integer Calculator(int firstNum, int secondNum, char operator) {
+        //에러일 경우 null 로 값을 표시해주기 위해 Integer 타입 변수 선언
         Integer result = 0;
-        switch (operater) {
+        //operator 에 따라 사칙 연산 계산
+        switch (operator) {
             case '+':
-                result =   a + b;
+                result =   firstNum + secondNum;
                 break;
             case '-':
-                result =   a - b;
+                result =   firstNum - secondNum;
                 break;
             case '*':
-                result = a * b;
+                result = firstNum * secondNum;
                 break;
             case '/':
-                if (b == 0) {
+                if (secondNum == 0) {//만약 0으로 나누는 경우 에러 메세지 출력 및 null 값 리턴
                     System.out.println("나눗셈 연산은 분모(두번째 정수)에 입력 될 수 없습니다.");
+                    //잘못된 연산의 경우 resultHistory에 값을 저장하지 않고 바로 return
                     return null;
                 }
-                result = a / b;
+                result = firstNum / secondNum;
             default:
                 System.out.println("올바른 연산을 입력해주세요.");
+                //잘못된 연산의 경우 resultHistory에 값을 저장하지 않고 바로 return
                 return null;
         }
+        //계산 결과를 resultHistory 에 저장
         resultHistory.add(result);
         return result;
     }
 
+    //가장 먼저 계산한 결과를 지우는 메서드
     public void removeResult(){
         resultHistory.remove(0);
     }
--- src/main/java/hello/LV2/Main.java
+++ src/main/java/hello/LV2/Main.java
@@ -20,11 +20,14 @@ public class Main {
 
             Calculator cal = new Calculator();
             Integer result = cal.Calculator(inputA, inputB, operater);
+            //에러가 나지 않은 정상 값만 출력하는 동작
             if (result != null) {
                 System.out.println("결과는 " + result + " 입니다");
             }
             System.out.println("다시 진행하시겠습니까? (exit 입력 시 종료)");
             System.out.println("가장 먼저 계산된 결과를 지우시겠습니까? (remove 입력 시 삭제)");
+            //입력값이 exit인 경우 반복문 탈출
+            //입력값이 remove인 경우 Calculator 객체의 제일 먼저 계산한 저장 결과 삭제
             String command = sc.nextLine();
             if (command.equals("exit")) break;
             if(command.equals("remove")) {cal.removeResult();}
