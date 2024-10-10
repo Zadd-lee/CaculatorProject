52f8aa5d6b56f8125f8b0632a38843ef556a0a50
이자드 <kimminkyeong0418@gmail.com>
2024-10-10 13:37:37 +0900

    refactor: LV1 변수명, 주석 설명 추가

--- src/main/java/hello/LV1/Main.java
+++ src/main/java/hello/LV1/Main.java
@@ -8,46 +8,44 @@ public class Main {
         Scanner sc = new Scanner(System.in);
 
         while (true) {
+            //값 받아오기
             System.out.println("첫번째 숫자를 입력하세요");
-            int inputA = sc.nextInt();
+            int inputFirst = sc.nextInt();
             System.out.println("두번째 숫자를 입력하세요");
-            int inputB = sc.nextInt();
+            int inputSecond = sc.nextInt();
             //버퍼 지우기
             sc.nextLine();
             System.out.println("사칙 연산을 입력하세요");
             char operater = sc.nextLine().charAt(0);
 
+            //결과값을 담을 변수 선언 및 초기화
             int result = 0;
 
+            //operater에 따라 사칙 연산 수행
             switch (operater) {
                 case '+':
-                    result = inputA + inputB;
+                    result = inputFirst + inputSecond;
                     break;
                 case '-':
-                    result = inputA - inputB;
+                    result = inputFirst - inputSecond;
                     break;
                 case '*':
-                    result = inputA * inputB;
+                    result = inputFirst * inputSecond;
                     break;
                 case '/':
-                    if (inputB == 0) {
+                    if (inputSecond == 0) {//0으로 나누는 경우, 에러 메세지 출력
                         System.out.println("나눗셈 연산은 분모(두번째 정수)에 입력 될 수 없습니다.");
                         break;
                     }
-                    result = inputA / inputB;
+                    result = inputFirst / inputSecond;
                     break;
             }
 
 
             System.out.println("결과는 " + result + " 입니다");
             System.out.println("다시 진행하시겠습니까? (exit 입력시 종료)");
-
+            //값이 exit 인 경우 반복문 탈출
             if (sc.nextLine().equals("exit")) break;
         }
-
-        //String 값이 수식이 맞는지 확인
-
-        //값을 계산기로 넣음
-        //값을 출력함
     }
 }
